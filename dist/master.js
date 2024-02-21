bac8eaba1d300dbc35d2150cf48500a07ea792e3
mhmd <ms.mhmdsalah@gmail.com>
2024-02-21 22:44:43 +0200

    in progress

--- dist/master.js
+++ dist/master.js
@@ -1,4 +1,9 @@
 "use strict";
+// check if thers local stroage color 
+let mainColors = localStorage.getItem("color_option");
+if (mainColors !== null) {
+    document.documentElement.style.setProperty("--main-color", localStorage.getItem("color_option"));
+}
 // toogle spin class on icon 
 let toggleSetting = document.querySelector(".togglle-settings i ");
 let settingsBox = document.querySelector(".settings-box");
@@ -6,8 +11,23 @@ toggleSetting.onclick = function () {
     // toggle class for icon 
     toggleSetting.classList.toggle("fa-spin");
     settingsBox.classList.toggle("open");
-    landingPage.classList.toggle("blur");
 };
+// switch color 
+const colorsLi = document.querySelectorAll(".colors-list li");
+colorsLi.forEach(li => {
+    li.addEventListener("click", (e) => {
+        if (!(e.target instanceof HTMLElement)) {
+            return;
+        }
+        else {
+            // set color on root 
+            const co = e.target.dataset.color;
+            document.documentElement.style.setProperty("--main-color", co);
+            localStorage.setItem("color_option", co);
+        }
+    });
+});
+// select landing page element 
 let landingPage = document.querySelector(".landing-page");
 // get Array of imgs 
 let imgsArray = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
--- src/master.ts
+++ src/master.ts
@@ -1,3 +1,10 @@
+// check if thers local stroage color 
+let mainColors = localStorage.getItem("color_option")
+
+if (mainColors !== null) {
+  document.documentElement.style.setProperty("--main-color", localStorage.getItem("color_option"))
+}
+
 // toogle spin class on icon 
 let toggleSetting = document.querySelector(".togglle-settings i ") as HTMLElement;
 let settingsBox = document.querySelector(".settings-box") as HTMLDivElement;
@@ -5,9 +12,26 @@ toggleSetting.onclick = function () {
   // toggle class for icon 
   toggleSetting.classList.toggle("fa-spin");
   settingsBox.classList.toggle("open")
-  landingPage.classList.toggle("blur")
 }
 
+// switch color 
+const colorsLi = document.querySelectorAll(".colors-list li") ;
+colorsLi.forEach(li => {
+  li.addEventListener("click", (e) => {
+      if (!(e.target instanceof HTMLElement)) {
+        return;
+      } else {
+        // set color on root 
+        const co:any = e.target.dataset.color
+        document.documentElement.style.setProperty("--main-color", co);
+        localStorage.setItem("color_option", co)
+      }
+  })
+})
+
+
+
+// select landing page element 
 let landingPage = document.querySelector(".landing-page") as HTMLElement;
 // get Array of imgs 
 let imgsArray: string[] = ["01.jpg", "02.jpg", "03.jpg", "04.jpg", "05.jpg"];
@@ -22,3 +46,4 @@ setInterval(()=>{
 landingPage.style.backgroundImage = `url("imgs/` + imgsArray[randomNumber] + `")`;
 
 }, duration);
+
